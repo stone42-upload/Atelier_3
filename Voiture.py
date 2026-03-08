28c17bf5f8204f41887398fcd7c65232fd293570
stone42-upload <arielstone883@gmail.com>
2026-03-07 20:51:48 -0500

    Implémentation des méthode de la classe Voiture

--- Voiture.py
+++ Voiture.py
@@ -3,3 +3,6 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
+
+    def afficherinformation(self):
+        print(f"matricule: {self.matricule}, marque:{self.marque}, couleur:{self.couleur}")
