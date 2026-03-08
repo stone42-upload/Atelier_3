425458114f63322281d5709e40f8ece0366847e3
stone42-upload <arielstone883@gmail.com>
2026-03-07 21:44:02 -0500

    Implémentation de la méthode entrerVoiture()

--- Parc.py
+++ Parc.py
@@ -1,4 +1,16 @@
+from Voiture import Voiture
 class Parc:
     def __init__(self, capacite,):
         self.capacite = capacite
-        self,voiture = []
+        self.voitures = []
+
+    def entrerVoiture(self, voiture):
+        if voiture in self.voitures:
+            print("Cette voiture est déja dans le parc")
+        elif len(self.voitures) < self.capacite:
+            self.voitures.append(voiture)
+            print("Cette voiture a été ajouter au parc")
+        else:
+            print("Désolé il n'y a pas assez de place dans ce parc")
+
+
